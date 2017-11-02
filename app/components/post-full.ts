99e5a1dbea150f66661faaaeef779ca219fcdbd1
collinadams <collinadams04@gmail.com>
2017-11-02 16:31:47 -0700

    First attempt at exercise 11

--- app/components/post-full.ts
+++ app/components/post-full.ts
@@ -1,7 +1,18 @@
 import Ember from 'ember';
+import stateFor from 'ember-state-services/state-for';
 
 const { Component } = Ember;
 
 export default Component.extend({
-  classNames: ['post-full']
+  classNames: ['post-full'],
+  
+  tagName: 'form',
+  
+  data: stateFor('draft-name', 'comment'),
+
+  actions: {
+    submitForm() {
+      this.get('comment').setProperties(this.get('data'));
+    }
+  }
 });
